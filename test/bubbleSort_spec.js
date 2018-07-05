a9d950891ab0e79c1e1ad6f841ec078fcc17f70b
Nishtha Malhotra <nishthamalhotra@gmail.com>
2018-07-05 19:14:28 -0400

    Add Test

--- test/bubbleSort_spec.js
+++ test/bubbleSort_spec.js
@@ -1,32 +1,36 @@
 const expect = require('chai').expect;
-const bubbleSort = require('../src/bubbleSort');
+const BubbleSort = require('../src/bubbleSort');
 const sinon = require('sinon');
 
 describe('Bubble Sort' , () => {
+    it('exists', () => {
+        expect(BubbleSort.sort).to.be.ok;
+    });
+
     it('Handles an empty array', () => {
-        expect(bubbleSort.sort([])).to.eql([]);
+        expect(BubbleSort.sort([])).to.eql([]);
     });
 
     it('sorts array in ascending order', () => {
-        expect(bubbleSort.sort([2,60,49,-1,0], (first, second) => { return first > second;})).to.eql([-1,0,2,49,60]);
+        expect(BubbleSort.sort([2,60,49,-1,0], (first, second) => { return first > second;})).to.eql([-1,0,2,49,60]);
     });
 
     it('sorts array in descending order', () => {
-        expect(bubbleSort.sort([2,60,49,-1,0], (first, second) => { return first < second;})).to.eql([60,49,2,0,-1]);
+        expect(BubbleSort.sort([2,60,49,-1,0], (first, second) => { return first < second;})).to.eql([60,49,2,0,-1]);
     });
 
     it('sorting an already sorted array does comparisons (n-1) times', () => {
         let count = 0;
-        expect(bubbleSort.sort([-1,0,2,49,60], (first, second) => { 
+        expect(BubbleSort.sort([-1,0,2,49,60], (first, second) => { 
             count++;
             return first > second;
         })).to.eql([-1,0,2,49,60]);
-        expect(count).to.eql(4);
+        expect(count).to.equal(4);
     });
 
     it('sorting an already sorted array does swaps 0 times (using spy)', () => {
-        const spy = sinon.spy(bubbleSort, "swap");
-        bubbleSort.sort([-1,0,2,49,60], (first, second) => { return first > second; });
+        const spy = sinon.spy(BubbleSort, "swap");
+        BubbleSort.sort([-1,0,2,49,60], (first, second) => { return first > second; });
         expect(spy.callCount).to.equal(0);
     });
 });
